1b15785c2f4bf77213d367f668fc50a76412b7a7
Eudes Carvalho <41842652+netoopz@users.noreply.github.com>
2024-05-13 17:15:38 -0300

    Update index.php
    bugs fixed

--- index.php
+++ index.php
@@ -38,8 +38,6 @@ include_once("conexao.php");
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 
-    <?php include_once("conexao.php"); ?>
-
 </head>
 <body>
     <main>
